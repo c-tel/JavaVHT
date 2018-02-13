156a51454adc291f31aff9e75f8079b80d3c5844
Stas Telizhenko <alex@alexcp.com>
2018-02-14 01:17:00 +0200

    test

--- Practice2.1/src/Gardener.java
+++ Practice2.1/src/Gardener.java
@@ -8,7 +8,7 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Gardener {
-
+//test collaboration
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		PrintWriter out = new PrintWriter(System.out, true);
